fe0369d022a42f3b6555ea74affcb2948de98457
midasmsc <midasmsc@gmail.com>
2024-01-08 18:52:37 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+//Needle7 needle;
+//    @Autowired
+//    public void setNeedle(Needle7 needle) {
+//        this.needle = needle;
+//    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " ;
+    }
+
+
+}
